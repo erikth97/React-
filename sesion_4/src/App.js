445d28b9675abcd4d1cedd3642d447b22e22edf5
Erik Tamayo <erik.tamayohe@uanl.edu.mx>
2023-06-02 12:29:51 -0600

    Create App.js

---
+++ sesion_4/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import Ejemplo1 from './hooks/Ejemplo1';
+import Ejemplo2 from './hooks/Ejemplo2';
+import MiComponenteConContexto from './hooks/Ejemplo3'
+import Ejemplo4 from './hooks/Ejemplo4';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {/* Ejemplos de uso de HOOKS */}
+        {/* <Ejemplo1></Ejemplo1> */}
+        {/* <Ejemplo2></Ejemplo2> */}
+        {/* <MiComponenteConContexto></MiComponenteConContexto> */}
+        {/* Todo loq ue hay aquí, es tratado como props.children */}
+        {/* <Ejemplo4 nombre="Martín">
+          <h3>
+            Contenido del props.children
+          </h3>
+        </Ejemplo4> */}
+        {/* <Greetingstyled name="Erik"></Greetingstyled> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
