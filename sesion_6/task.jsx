fcce7f9dd6f6f4cdf170e20007e63ada0924af64
Erik Tamayo <erik.tamayohe@uanl.edu.mx>
2023-06-06 10:37:15 -0600

    Create task.jsx

---
+++ sesion_6/task.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Task } from '../../models/task.class';
+
+//importamos la hoa de estilos de task.scss
+import '../../styles/task.scss';
+
+
+const TaskComponent = ({ task }) => {
+    
+    useEffect(() => {
+        console.log('Created Task')
+        return () => {
+            console.log(`Task: ${task.name} is going to unmount`)
+        };
+    }, [task]);
+     
+    return (
+        <div>
+            <h2 className='task-name'>
+                Nombre:        { task.name }
+            </h2>
+            <h3>
+                Description:   { task.description }
+            </h3>
+            <h4>
+                Level:         { task.level }
+            </h4>
+            <h5>
+                This task is:  { task.completed ? 'COMPLETED':'PENDING'}
+            </h5>
+
+        </div>
+    );
+};
+
+
+TaskComponent.propTypes = {
+      task: PropTypes.instanceOf(Task)
+};
+
+
+export default TaskComponent;
