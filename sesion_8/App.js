d969c8fd4bd9e8b326d555cbe34cb28b263b380f
Erik Tamayo <erik.tamayohe@uanl.edu.mx>
2023-06-08 12:13:46 -0600

    Create App.js

---
+++ sesion_8/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import Greeting from './components/pure/greeting';
+import Greetingf from './components/pure/greetingF';
+import TaskListComponent from './components/container/task_list';
+import GreetingStyled from './components/pure/greetingStyled';
+import Father from './components/container/father';
+
+function App() {
+  return (
+    <div className="App">
+        {/*<header className="App-header">*/}
+          {/*<img src={logo} className="App-logo" alt="logo" />*/}
+          {/* Componente propio Greeting.jsx      */}
+          {/* <Greeting name={"Erik"}></Greeting> */}
+          {/* Componente de ejemplo funcional  */}           
+          {/*<Greetingf name="Erik"></Greetingf>  */} 
+          {/* Componente de listado de Tareas     */}           
+          {/* <TaskListComponent></TaskListComponent> */}
+          {/* <GreetingStyled name="Erik"></GreetingStyled> */}
+        {/*</header>*/}
+          {/* Gestion de Eventos */}
+          <Father></Father>        
+          {/* <TaskListComponent></TaskListComponent> */}
+    </div>
+  );
+}
+
+export default App;
