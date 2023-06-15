6c5541fff3d5d962829773d2af4e9014145b13dd
Erik Tamayo <erik.tamayohe@uanl.edu.mx>
2023-06-15 08:38:33 -0600

    Create App.js

---
+++ ejercicio_sesiones-7-8-9/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import ContactoContainer from "./components/containers/contacto-container";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ContactoContainer />
+      </header>
+    </div>
+  );
+}
+
+export default App;
