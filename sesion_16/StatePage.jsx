e865a25944043dc5d5b4e1aef69a155fb73c2db2
Erik Tamayo <erik.tamayohe@uanl.edu.mx>
2023-07-06 09:42:44 -0600

    Create StatePage.jsx

---
+++ sesion_16/StatePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+
+    const Statepage = () =>{
+
+    return (
+        <div>
+            
+        </div>
+    
+    );
+}
+
+export default Statepage;
